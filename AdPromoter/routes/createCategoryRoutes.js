5d3000324bbc1f2fc213a817aa0587ba633e5d04
icyatwa <icyatwandoba@gmail.com>
2025-07-22 00:10:40 +0200

    data structure

--- AdPromoter/routes/createCategoryRoutes.js
+++ AdPromoter/routes/createCategoryRoutes.js
@@ -4,12 +4,9 @@ const router = express.Router();
 const categoryController = require('../controllers/createCategoryController');
 const authMiddleware = require('../../middleware/authMiddleware');
 
-// All category routes require authentication
 router.use(authMiddleware);
 
-// Create a new category
 router.post('/', categoryController.createCategory);
-
 router.put('/:categoryId/reset-user-count', categoryController.resetUserCount);
 router.delete('/:categoryId', categoryController.deleteCategory);
 router.get('/', categoryController.getCategories);
@@ -17,15 +14,7 @@ router.get('/:websiteId/advertiser', categoryController.getCategoriesByWebsiteFo
 router.get('/:websiteId', categoryController.getCategoriesByWebsite);
 router.get('/category/:categoryId', categoryController.getCategoryById);
 router.patch('/category/:categoryId/language', categoryController.updateCategoryLanguage);
-
 router.get('/pending/:ownerId', categoryController.getPendingAds);
-router.get('/mixed/:userId', categoryController.getUserMixedAds);
-router.get('/pending-ad/:adId', categoryController.getPendingAdById);
 router.put('/approve/:adId/website/:websiteId', categoryController.approveAdForWebsite);
-router.put('/approve/:adId', categoryController.approveAd);
-router.get('/approved-ads', categoryController.getApprovedAds);
-router.get('/approved/:ownerId', categoryController.getApprovedAdsByUser);
-router.get('/ad-details/:adId', categoryController.getAdDetails);
-router.put('/confirm/:adId/website/:websiteId', categoryController.confirmWebsiteAd);
 
 module.exports = router;
