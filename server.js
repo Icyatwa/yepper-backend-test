10994c92182aefdea0b8bf4fea2c804ebeacd116
icyatwa <icyatwandoba@gmail.com>
2026-05-31 10:10:58 +0200

    changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const passwordRoutes = require('./routes/passwordRoutes');
 
 // AdOwner
 const webAdvertiseRoutes = require('./AdOwner/routes/WebAdvertiseRoutes');
-const adminRoutes = require('./admin/routes/adminRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express(); // ← INITIALIZE APP FIRST
 
